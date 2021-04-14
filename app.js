4fb83f5f8be378f6bd6e9f28afcccd98b9a4f3bb
majdmasadeh <>
2021-04-14 23:37:22 +0000

    adding java script

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log('Welcome to Cosmatica shop');
+alert('Welcometo Cosmatica shop');
+var userlist=prompt('Please type your list name ');
+var userproduct=prompt('(Please type your product)');
+console.log('user list'+ userlist);
+console.log('user product' + userproduct);
+if(userlist =='skincare')
+{
+  var image = '<img src="https://anamalika.com/wp-content/uploads/2019/03/%D9%83%D8%B1%D9%8A%D9%85-%D8%A7%D8%B3%D8%A7%D8%B3-studio-fix-fluid-%D9%85%D9%86-%D9%85%D8%A7%D8%B1%D9%83%D8%A9-%D9%85%D8%A7%D9%83.jpg" alt= "skincare creams"/>';
+  
+  document.write(image);
+  {alert('skincare crems')}
+
+}else if(userlist =='lips'){
+  var image1 = '<img src="https://www.sayidaty.net/sites/default/files/2018/12/06/4622716-206623972.jpg" alt= "lips stick"/>';
+  
+  document.write(image1);}
+  {alert('lips stick')}
